0a972ab185d79d85a4ee94b5c8d58219e5ce1856
Slpn <75524609+Slpn@users.noreply.github.com>
2021-03-22 21:49:22 +0100

    Add files via upload

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -2,8 +2,5 @@
 
 int ft_isalnum(int c)
 {
-    if ((c >= 'a' && c <= 'z' ) || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'))
-        return (1);
-    else
-        return (0);
+    return ((c >= 'a' && c <= 'z' ) || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'));
 }
